f45ac8e5ece203642166c56d0bb41679f2b7d10e
Travis CI <ghoul.ahmed5@gmail.com>
2019-09-09 22:39:25 +0100

    Deploy to GitHub Pages

--- sw.js
+++ sw.js
@@ -1,1 +1,36 @@
-const cacheName="portfolio-4",toCache=["./index.html","./main.css","./main.js","./images/img.png"];self.addEventListener("install",a=>{a.waitUntil(caches.open(cacheName).then(a=>a.addAll(toCache)).then(()=>self.skipWaiting()))}),self.addEventListener("activate",a=>{a.waitUntil(self.clients.claim()),a.waitUntil(caches.keys().then(a=>Promise.all(a.map(a=>a.startsWith("portfolio")&&a!==cacheName?caches.delete(a):a))))}),self.addEventListener("fetch",a=>{a.respondWith(caches.match(a.request).then(b=>b||fetch(a.request)))});
+const cacheName = "portfolio-4";
+const toCache = [
+    "./index.html",
+    "./main.css",
+    "./main.js",
+    "./images/img.png",
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => cache.addAll(toCache))
+        .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(self.clients.claim());
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.map(key => {
+                if (key.startsWith("portfolio") && key !== cacheName) {
+                    return caches.delete(key);
+                }
+                return key;
+            }));
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+});
